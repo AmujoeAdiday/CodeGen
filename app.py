096d0710c59afe6ac8d1130553b4ad3953314bed
AmujoeAdiday <51425186+AmujoeAdiday@users.noreply.github.com>
2025-10-01 21:53:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,38 +2,34 @@ import streamlit as st
 
 st.title("Excel Formula Replacer Tool")
 
-col12wk, colBestModel, colRight = st.columns([1, 1, 1])
+colLeft, colCenter, colRight = st.columns([1,1,1])
 
-with col12wk:
-    cell_12wk = st.text_input("Cell of Value of 12 WK MA", value="", placeholder="e.g. AN26")
-    cell_8wk = st.text_input("Cell of Value of 8 WK MA", value="", placeholder="e.g. AO26")
-    cell_4wk = st.text_input("Cell of Value of 4 WK MA", value="", placeholder="e.g. AP26")
+with colLeft:
+    val_12wk = st.text_input("Value of 12 WK MA Cell", value="", placeholder="e.g. AN26")
+    val_8wk = st.text_input("Value of 8 WK MA Cell", value="", placeholder="e.g. AO26")
+    val_4wk = st.text_input("Value of 4 WK MA Cell", value="", placeholder="e.g. AP26")
 
-with colBestModel:
-    cell_best_model = st.text_input("Cell of Best Model Indicator", value="", placeholder="e.g. AR26")
-    cell_item = st.text_input("Cell of Item Code", value="", placeholder="e.g. E26")
+with colCenter:
+    best_model = st.text_input("Best Model Indicator Cell", value="", placeholder="e.g. AR26")
+    item_code = st.text_input("Item Code Cell", value="", placeholder="e.g. E26")
 
 with colRight:
-    cell_updated_friday = st.text_input(
-        "Cell of Updated Last Friday (includes $)", value="", placeholder="e.g. AX$24"
-    )
-    cell_full_truck = st.text_input(
-        "Cell of # Weeks Full Truck (includes $)", value="", placeholder="e.g. AT$22"
-    )
+    last_friday = st.text_input("Updated Last Friday Cell (with $)", value="", placeholder="e.g. AX$24")
+    full_truck = st.text_input("# Weeks Full Truck Cell (with $)", value="", placeholder="e.g. AT$22")
 
 if st.button("Generate Formula"):
     formula = (
-        f'=IF({cell_best_model}="12 WK MA", {cell_12wk},'
-        f' IF({cell_best_model}="8 WK MA", {cell_8wk},'
-        f'  IF({cell_best_model}="4 WK MA", {cell_4wk},'
+        f'=IF({best_model}="12 WK MA", {val_12wk},'
+        f' IF({best_model}="8 WK MA", {val_8wk},'
+        f'  IF({best_model}="4 WK MA", {val_4wk},'
         f'   AVERAGE('
         f'    INDEX(\'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$AN,'
-        f'     MATCH({cell_item}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$A, 0),'
-        f'     MATCH({cell_updated_friday}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$1:$1, 0) + 1'
+        f'     MATCH({item_code}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$A, 0),'
+        f'     MATCH({last_friday}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$1:$1, 0) + 1'
         f'    ):'
         f'    INDEX(\'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$AN,'
-        f'     MATCH({cell_item}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$A, 0),'
-        f'     MATCH({cell_updated_friday}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$1:$1, 0) + {cell_full_truck}'
+        f'     MATCH({item_code}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$A:$A, 0),'
+        f'     MATCH({last_friday}, \'[Model_Suggestion.xlsx]Overall Suggestion\'!$1:$1, 0) + {full_truck}'
         f'    )'
         f'   )'
         f'  )'
